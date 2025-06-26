6274b21cb455b6976eba44ba12de86bedba41864
Pavel Mesenev <mesenev_pr@dvfu.ru>
2025-06-26 21:39:33 +1000

    Update create_cgroups.sh
    dockerized version of cgroups

--- create_cgroups.sh
+++ create_cgroups.sh
@@ -3,13 +3,10 @@
 CONTROL_GROUPS="sp"
 SUBS="blkio cpuacct memory pids freezer"
 
-sudo -v
-
 for cgroup in ${CONTROL_GROUPS}
 do
 	for sub in ${SUBS}
 	do	
-		sudo mkdir /sys/fs/cgroup/$sub/$cgroup/
-		sudo chown -R ${USER} /sys/fs/cgroup/$sub/$cgroup/
+		mkdir /sys/fs/cgroup/$sub/$cgroup/
 	done
 done
